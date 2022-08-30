68caf01f48a5c82cb9f4f3fe285067c56fa8017a
Arif Mukhtarov <departed2015@gmail.com>
2022-08-30 14:50:59 +0400

    project ready

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,8 @@
+import imp
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-
+from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -13,6 +14,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     
+    
     from .views import views
     from .auth import auth
     
@@ -22,6 +24,15 @@ def create_app():
     from .models import User, Note
     
     create_database(app)
+    
+    
+    login_manager = LoginManager()
+    login_manager.login_view ='auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     
     return app
 
--- website/static/index.js
+++ website/static/index.js
@@ -1,1 +1,8 @@
-console.log('Javascript from static folder');
+function deleteNote(noteId){
+    fetch('/delete-note',{
+        method: 'POST',
+        body: JSON.stringify({noteId: noteId}),
+    }).then((_res)=>{
+        window.location.href ="/"
+    });
+}
